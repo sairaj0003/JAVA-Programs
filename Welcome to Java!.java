6bf724a0a36558573684abe35c3e947954ddfaca
SAI RAJ GUDURU <140234339+sairaj0003@users.noreply.github.com>
2023-10-14 13:57:02 +0530

    Welcome to Java!

---
+++ Welcome to Java!.java
@@ -0,0 +1,11 @@
+/*
+Printing to console
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.print("Hello, Java.");
+    }
+}
